a5881cb7a17ebe338ca8c4a30383a3df5f0fa270
백건우 <qorrjsdn668@daum.net>
2023-02-15 03:36:06 +0000

    컨테이너 변경 작업 2023.02.15

--- python/docx.py
+++ python/docx.py
@@ -1,5 +1,7 @@
-pip install oauth2client
-pip install gspread
+###모듈 설치 명령어
+# python -m pip install oauth2client
+# python -m pip install gspread
+# python -m pip install --upgrade pip
 
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
@@ -14,8 +16,10 @@ json_key_path = "../META-INF/api_key.json"	# JSON Key File Path
 credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
 gc = gspread.authorize(credential)
 
-import docx
-from docx.shared import Pt
+# URL로 열기
+docx_url = "https://docs.google.com/spreadsheets/d/18n328MQoyReTNKuvvs_3YgLO-JW-bGWodQcKZen63LA/edit#gid=0"
+doc = gc.open_by_url(docx_url)
+
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 templ = docx.Document('/storyboard/story.docx')
 for x, paragraph in enumerate(templ.paragraphs):
